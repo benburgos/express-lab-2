41e118428c9441b76bb6a9aefd47238e82400a1c
Ben Burgos <ben.a.burgos@gmail.com>
2022-04-02 17:09:56 -0700

    Tip Calculator express application created.

--- server.js
+++ server.js
@@ -3,7 +3,11 @@ const app = express();
 const PORT = 3000;
 
 app.get('/greeting/:name', (req, res) => {
-    res.send(`Woah! Hey there, ${req.params.name}`)
+    res.send(`Woah! Hey there, ${req.params.name}!`)
+});
+
+app.get('/tip/:total/:tipPercentage', (req, res) => {
+    res.send(`Your tip should be $${Math.round((req.params.tipPercentage / req.params.total) * 100)} dollars.`)
 });
 
 app.listen(PORT)
